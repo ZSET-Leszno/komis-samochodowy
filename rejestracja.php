e4a0eaee5cd035486f41f881c1bf8de2f2483216
borysjjankowiak <borysjjankowiak@wp.pl>
2023-03-07 10:53:23 +0100

    1

---
+++ rejestracja.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpeedyMotors</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.png">
+</head>
+<body>
+    <div class="nav-logowanie">
+    <a href="index.html"><h1 class="logo">SpeedyMotors</h1></a>
+    </div>
+    <div class="panel">
+        <div class="logo-foto">
+
+        </div>
+        <div class="logo-form">
+            <h3>Zarejestruj się</h3>
+            <form>
+                <input class="log-input" id="login" placeholder="Podaj login"><br>
+                <input type="email" class="log-input" id="haslo"placeholder="Podaj email"><br>
+                <input type="password" class="log-input" id="haslo"placeholder="Podaj hasło"><br>
+                <input type="password" class="log-input" id="haslo"placeholder="Powtórz hasło"><br>
+                <div class="zgody">
+                <input type="checkbox" class="zgoda"><span>Oświadczam, iż zapoznałem się z treścią Regulaminu.<br></span>
+                <input type="checkbox" class="zgoda"><span>Akceptuję politykę przetwarzania danych osobowych.</span>
+                </div>
+
+                <button type="submit" class="przycisk">ZAREJESTRUJ SIĘ</button><br><br>
+                <p>Masz już konto? <a href="rejestracja.php">Zaloguj się</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
