a2cc384801a2cdc0548943e57bdfc58e7b60118c
Admin <jakobso2003@gmail.com>
2023-03-08 16:51:47 +0100

    nie działa nic

--- funkcje.php
+++ funkcje.php
@@ -6,7 +6,21 @@ function rejestracja(){
     }
     else{
         $conn=new mysqli('localhost', 'root', '', 'komis');
-        if($conn->query('SELECT * '))
+        if($conn->query('SELECT count(id_uzytkownika) from uzytkownicy where login="'.$_POST['login'].'"'))
+        {
+            echo "Login jest już zajęty";
+        }
+        elseif($var=$conn->query('SELECT * from uzytkownicy where email="'.$_POST['mail'].'"')->num_rows!=0)    
+        {
+            echo "Istnieje już konto dla podanego adresu e-mail";
+            echo $var;
+        }
+        else{
+            if($conn->query('INSERT INTO uzytkownicy(NULL, "'.$_POST['mail'].'", "'.$_POST['login'].'", "'.hash('sha256', $_POST['haslo']).'", "", "", ""')){
+                echo "Rejestracja udana";
+            }
+
+        }
     }
 }
 rejestracja();
