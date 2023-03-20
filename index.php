4e7959f034b156d67278b5bb763ba51c3dc327f0
borysjjankowiak <borysjjankowiak@wp.pl>
2023-03-20 21:24:10 +0100

    proponowane oferty

--- index.php
+++ index.php
@@ -4,33 +4,75 @@
 ?>
 <div class="main">
     <div class="parent">
-        <div class="div1"> <span><p>Samochód na raty - <br>Decyzja w 30 minut </p><button onclick="location.href = '#'">Sprawdź ofertę</button></span></div>
+        <div class="div1"> <span><p>Samochód na raty - <br>Decyzja w 30 minut </p><button class="guzik" onclick="location.href = 'oferty.php'">Sprawdź ofertę</button></span></div>
         <div class="div2">  <p>Auta już od 3999 zł</p></div>
         <div class="div3">  <p>Satysfakcja gwarantowana</p></div>
     </div>
     
+
+    <div class="oferty">
+        <h2>Wybrane dla Ciebie</h2>
+    <div class="wrapper-oferty">
+        <div class="oferta">
+            <a href="#">
+            <div class="oferta-foto" style="background-image: url(img/1_1.webp)"></div>
+            <h4>Citroen C5 2991 </h4>
+            <p>2010 • 15000 km • Diesel • 1999 cm3</p>
+            <span>20000 PLN</span>
+        </a> 
+        </div>
+        <div class="oferta">
+            <a href="#">
+            <div class="oferta-foto" style="background-image: url(img/1_1.webp)"></div>
+            <h4>Citroen C5 2991 </h4>
+            <p>2010 • 15000 km • Diesel • 1999 cm3</p>
+            <span>20000 PLN</span>
+        </a> 
+        </div>
+        <div class="oferta">
+            <a href="#">
+            <div class="oferta-foto" style="background-image: url(img/1_1.webp)"></div>
+            <h4>Citroen C5 2991 </h4>
+            <p>2010 • 15000 km • Diesel • 1999 cm3</p>
+            <span>20000 PLN</span>
+        </a> 
+        </div>
+        <div class="oferta">
+            <a href="#">
+            <div class="oferta-foto" style="background-image: url(img/1_1.webp)"></div>
+            <h4>Citroen C5 2991 </h4>
+            <p>2010 • 15000 km • Diesel • 1999 cm3</p>
+            <span>20000 PLN</span>
+        </a> 
+        </div>
+    </div>
+    <button class="guzik2" onclick="location.href = 'oferty.php'">Zobacz wszystkie</button>
+    </div>
+    <div class="staty">
     <div class="wrapper">
         <div class="container">
         <i class="fa-solid fa-car-rear"></i>
-        <span class="num" data-val="95">000</span>
+        <span class="num" data-val="95">0</span>
         <span class="text">Aut w ofercie</span>
         </div>
         <div class="container">
         <i class="fa-solid fa-handshake"></i>
-        <span class="num" data-val="120">000</span>
+        <span class="num" data-val="120">0</span>
         <span class="text">Sprzedanych aut</span>
         </div>
         <div class="container">
         <i class="fa-solid fa-calendar-days"></i>
-        <span class="num" data-val="16">000</span>
+        <span class="num" data-val="16">0</span>
         <span class="text">Lat na rynku</span>
         </div>
         <div class="container">
         <i class="fa-solid fa-face-smile"></i>
-        <span class="num" data-val="115">000</span>
+        <span class="num" data-val="115">0</span>
         <span class="text">Zadowolonych klientów</span>
         </div>
     </div>
+    </div>
+    
 
 </div>
 
--- skrypty.js
+++ skrypty.js
@@ -1,9 +1,9 @@
-//let x=0;
-//window.onscroll = function() {scrollFunction()};
-//        function scrollFunction() {
- //       //tu możesz zmieniać po ilu pikselach ma się odliczać
- //       if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 && x==0) {liczby(); x=1;}}
-//function liczby(){
+let x=0;
+window.onscroll = function() {scrollFunction()};
+       function scrollFunction() {
+       //tu możesz zmieniać po ilu pikselach ma się odliczać
+       if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500 && x==0) {liczby(); x=1;}}
+function liczby(){
     let valueDisplays = document.querySelectorAll(".num");
     let interval = 2000;
 
@@ -19,4 +19,4 @@
             }
         }, duration);
     });
-//};
+};
