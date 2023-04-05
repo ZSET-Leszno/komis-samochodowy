63cc45c9dec24901b09d5efa2eca6fc36d575687
Admin <jakobso2003@gmail.com>
2023-04-05 16:34:55 +0200

    wysylanie maili

--- funkcje.php
+++ funkcje.php
@@ -142,7 +142,7 @@ function rejestracja($login, $mail, $haslo, $powtorz_haslo){
             $maail->IsSMTP();
             $maail->CharSet="UTF-8";
             $maail->Host = "smtp.gmail.com"; /* Zależne od hostingu poczty*/
-            $maail->SMTPDebug = 2;
+            $maail->SMTPDebug = 0;
             $maail->Port = 587 ; /* Zależne od hostingu poczty, czasem 587 */
             $maail->SMTPSecure = 'tsl'; /* Jeżeli ma być aktywne szyfrowanie SSL */
             $maail->SMTPAuth = true;
