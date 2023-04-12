25ec5c1629eb37c33b3dd564aa6ed0e41a95df77
Admin <jakobso2003@gmail.com>
2023-04-12 15:56:30 +0200

    ip

--- funkcje.php
+++ funkcje.php
@@ -149,7 +149,7 @@ function rejestracja($login, $mail, $haslo, $powtorz_haslo){
             $maail->IsHTML(true);
             $maail->Username = "speedymotorsinfo@gmail.com"; /* login do skrzynki email często adres*/
             $maail->Password = "lahshfschlwtdwxy"; /* Hasło do poczty */
-            $maail->setFrom('speedyinfo@spoko.pl'); /* adres e-mail i nazwa nadawcy */
+            $maail->setFrom('speedymotorsinfo@gmail.com'); /* adres e-mail i nazwa nadawcy */
             $maail->AddAddress($mail); /* adres lub adresy odbiorców */
             $maail->Subject = "SpeedyMotors potwierdzenie rejestracji"; /* Tytuł wiadomości */
             $maail->Body = "Dziekujemy za utworzenie konta w naszym serwisie.<br>Aby potwierdzić rejestrację wejdź w ten link: 158.101.171.163/logowanie.php?p=$kod";
