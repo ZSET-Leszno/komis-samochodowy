5c6dd383161a4fe088beb9fa2dee549eea9ee4cb
borysjjankowiak <borysjjankowiak@wp.pl>
2023-04-14 10:29:02 +0200

    onas

--- onas.php
+++ onas.php
@@ -14,7 +14,7 @@
         
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629457.8866614322!2d16.470124294069965!3d51.95547616862436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4705bd542b432ec3%3A0xd30ec24fde7c25fa!2sAuto%20Komis!5e0!3m2!1spl!2spl!4v1681459275978!5m2!1spl!2spl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-    <div class="firma">
+    <div class="firma2">
         <div class="firma-text">
            <h2>Gdzie nas znajdziesz?</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugiat ea iusto aliquid blanditiis debitis beatae temporibus, magni similique cum odit iure consectetur molestias, quia ipsam corrupti dolore amet a!</p>
