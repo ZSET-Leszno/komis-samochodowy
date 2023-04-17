412d3cb44ee92ebe89536d45c3455ba90fff4dd2
borysjjankowiak <borysjjankowiak@wp.pl>
2023-04-17 22:45:32 +0200

    Co-authored-by: KubaMackowski <KubaMackowski@users.noreply.github.com> Co-authored-by: Jakub Kołodziej <JakubKolodziej6@users.noreply.github.com>

--- sprzedaj.php
+++ sprzedaj.php
@@ -6,10 +6,10 @@
 <div class="formularz">
     <div class="form">
     <h1>Stwórz własną ofertę sprzedaży</h1>
-    <form action='#' method='POST'>
+    <form id="ankieta" action='#' method='POST'>
         <input class="sell"type="text" name="marka" placeholder="Marka auta" required>
         <input class="sell"type="text" name="model" placeholder="Model auta" required>>
-        <input class="sell"type="number" name="moc" placeholder="Moc silnika  (w km)" minlength="2" required>>
+        <input class="sell"type="number" id="moc" name="moc" placeholder="Moc silnika  (w km)" minlength="2" required>>
         <input class="sell"type="number" name="rok" placeholder="Rok produkcji" maxlength="4" min="1700" max="2023" required>>
         <input class="sell"type="number" name="pojemnosc" placeholder="Pojemność silnika   (w cm3)" maxlength="4" required>>
         <input class="sell"type="number" name="cenka" placeholder="Proponowana cena  (w zł)" required><br>
@@ -23,6 +23,23 @@
     </form>
     </div>
 </div>
+<script>
+    function formularz() {
+            puste();
+            var form = document.getElementById("ankieta");
+
+            if (form.moc.value.trim() == "") {
+                document.getElementById("odp1").innerHTML = "To pole jest puste!";
+                test = false;
+            } else if (form.moc.value.trim().length < 3) {
+                document.getElementById("odp1").innerHTML = "Moc musi zawierać co najmniej 2 znaki!";   
+                test = false;
+            }
+    }
+    function puste() {
+            document.getElementById("odp1").innerHTML = ""; 
+    }
+</script>
 <?php
     echo stopka();
 ?>
